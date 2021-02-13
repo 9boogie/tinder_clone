2e2a8d9f1461bd54212c6a6e0f51ea71111bddce
9boogie <jzero.youngs89@gmail.com>
2021-02-13 02:53:06 -0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Tinder Clone App</h1>
+
+      {/* Header */}
+      <Header />
+
+      {/* Cards */}
+      {/* Buttons below cards */}
+
+      {/* Chats Screen */ }
     </div>
   );
 }
