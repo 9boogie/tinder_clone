09baeef8a3888dbbb0da8a77faa387f1d033f48a
9boogie <jzero.youngs89@gmail.com>
2021-02-16 02:33:03 -0800

    Create Tinder Cards

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import './App.css';
 import Header from './Header/Header';
+import TinderCards from './TinderCards/TinderCards';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Switch>
-          <Route path='/'>
-            <h1>I am homepage</h1>
-          </Route>
           <Route path='/chat'>
-            <h1>I am that chatpage</h1>
+            <h1>I am the chatpage</h1>
+          </Route>
+          <Route path='/'>
+            <TinderCards />
           </Route>
         </Switch>
-        <h1>Tinder Clone App</h1>
-
-        {/* Header */}
-        <Header />
-
-        {/* Cards */}
-        {/* Buttons below cards */}
-
-        {/* Chats Screen */ }
-
       </Router>
     </div>
   );
---
+++ src/TinderCards/TinderCards.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import TinderCard from 'react-tinder-card';
+import './TinderCards.css';
+
+function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      name: 'Harry Potter',
+      url: 'https://cnet2.cbsistatic.com/img/-5XzXScvtcX732sw1sYo_OCsI-8=/940x0/2016/09/19/79530bcb-7c95-4a67-bc33-2119c828e6e0/harry-potter-philosophers-stone.jpg'
+    },
+    {
+      name: 'Malfoy',
+      url: 'https://i.pinimg.com/736x/11/c5/a3/11c5a3dc93a313731e411fc0baf3e109.jpg'
+    }
+  ]);
+
+  // BAD WAY
+  // const people = [];
+  // people.push('tim', 'max');
+
+  // Good Way (React Way)
+  // setPeple([...people, 'tim', 'max']);
+
+  return (
+    <div>
+      <h1>Tinder Cards</h1>
+
+      {people.map(person => (
+        <TinderCard className="swipe" key={person.name} preventSwipe={['up', 'down']}>
+          <div style={{ backgroundImage: `url(${person.url})` }} className='card'>
+            <h3>{person.name}</h3>
+          </div>
+        </TinderCard>  
+      ))}
+    </div>
+  )
+}
+
+export default TinderCards
