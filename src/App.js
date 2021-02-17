ec5570180ffb42d294174cecb832bfdc44688ed2
9boogie <jzero.youngs89@gmail.com>
2021-02-17 15:56:06 -0800

    Chats List

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import TinderCards from './TinderCards/TinderCards';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SwipeButtons from './SwipeButtons/SwipeButtons';
 import Chats from './Chats/Chats';
+import ChatScreen from './Chats/ChatScreen';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path='/chat/:person'>
+            <Header backButton='/chat' />
+            <ChatScreen />
+          </Route>
           <Route path='/chat'>
             <Header backButton='/' />
             <Chats />
---
+++ src/Chats/ChatScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatScreen() {
+  return (
+    <div className='chatScreen'>
+      <h2>Chat Screen</h2>
+    </div>
+  )
+}
+
+export default ChatScreen;
